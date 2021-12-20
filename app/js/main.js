806699dc5f38ee8b486d56fb7e7417bcf4b633b0
skifozzze <skifozzze@gmail.com>
2021-12-20 13:38:09 +0200

    added product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 $(function () {
   $('.product-slider__inner').slick({
-    
+    // prevArrow: "<img src='../images/icons/left-arrow.svg' class='prev' alt='Left arrows.'>",
+    // nextArrow: "<img src='../images/icons/right-arrow.svg' class='next' alt='Right arrows.'>",    
   })
   
 })
