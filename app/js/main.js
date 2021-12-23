5697d646528798d979cde0a36d53fde8b4f17cd4
skifozzze <skifozzze@gmail.com>
2021-12-23 22:14:01 +0200

    added swiper

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
 $(function () {
+
+  
+  const swiper = new Swiper('.swiper', {  
+   
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  });
+
   
   $('.product-slider__inner').slick({  
+    fade: true,
   })
   
 })
--- gulpfile.js
+++ gulpfile.js
@@ -40,8 +40,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
-    // 'node_modules/swiper/swiper-bundle.js',
-
+    'node_modules/swiper/swiper-bundle.js',
     'node_modules/slick-carousel/slick/slick.js',    
     // 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     // 'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
